40f812c319dc27271bda0f53ac1c75e58d1f1df9
uherrmann <uherrmann@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-12 16:12:25 +0000

    0.2.0 Backup working draft
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cscgallery/trunk@65359 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,8 +10,8 @@ $_EXTCONF = unserialize($_EXTCONF); // unserializing the configuration so we can
 if ($_EXTCONF['removePositionTypes'] || !$_EXTCONF) {
 	t3lib_extMgm::addPageTSConfig('
 		TCEFORM.tt_content.imageorient.types.' . $_EXTKEY . '_pi1.removeItems = 17,18
-		TCEFORM.tt_content.imageorient.types.' . $_EXTKEY . '_pi1.altLabels.25 = Beside, right
-		TCEFORM.tt_content.imageorient.types.' . $_EXTKEY . '_pi1.altLabels.26 = Beside, left
+		TCEFORM.tt_content.imageorient.types.' . $_EXTKEY . '_pi1.altLabels.25 = LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:tt_content.imageorient.25
+		TCEFORM.tt_content.imageorient.types.' . $_EXTKEY . '_pi1.altLabels.26 = LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:tt_content.imageorient.26
 		TCEFORM.tt_content.imagecols.types.' . $_EXTKEY . '_pi1.addItems.9 = 9
 		TCEFORM.tt_content.imagecols.types.' . $_EXTKEY . '_pi1.addItems.10 = 10
 		TCEFORM.tt_content.imagecols.types.' . $_EXTKEY . '_pi1.addItems.11 = 11
